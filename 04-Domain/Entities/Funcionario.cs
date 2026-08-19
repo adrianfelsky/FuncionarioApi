94c400de704a267af7e4e24fc9ee383e5d6aaecd
Adrian <adriangazzani@gmail.com>
2026-08-19 16:52:07 -0300

    Criação da entidade Funcionario e interface IFuncionarioRepository.

---
+++ 04-Domain/Entities/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _04_Domain.Entities;
+
+public class Funcionario
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O cargo é obrigatório.")]
+    public string Cargo { get; set; } = string.Empty;
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "O salário deve ser maior que zero.")]
+    public decimal Salario { get; set; }
+
+    [Required(ErrorMessage = "O departamento é obrigatório.")]
+    public string Departamento { get; set; } = string.Empty;
+
+    public bool Ativo { get; set; } = true;
+}
---
+++ 04-Domain/Interfaces/IFuncionarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using _04_Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace _04_Domain.Interfaces;
+
+public interface IFuncionarioRepository
+{
+    Task<List<Funcionario>> GetAllAsync();
+    Task<Funcionario?> GetByIdAsync(int id);
+    Task<Funcionario> AddAsync(Funcionario funcionario);
+    void Update(Funcionario funcionario);
+    void Delete(Funcionario funcionario);
+    Task SaveChangesAsync();
+}
